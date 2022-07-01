4bf54869bd8bc3785cbd4ff76af83dbb1fffc9eb
Nikhil Bobade <nikhil.bobade@yash.com>
2022-07-01 17:17:26 +0530

    java 8 Assignment added

--- Java 8/Que_Of_Logical_13_14_15/Que_13_Series.java
+++ Java 8/Que_Of_Logical_13_14_15/Que_13_Series.java
@@ -1,28 +1,35 @@
 package Que_Of_Logical_13_14_15;
 
 public class Que_13_Series {
-public static void main(String[] args) {
-	int a[]= {1,2,3,6,7,8,9,10,11,12,13,14,17,18,19,20};
-	for(int i=0;i<a.length;i++) {
-		if(i<=4) {
-		System.out.println(" ===");
-			String b=a[i]+"-"+(a[i+2]);
-			System.out.print(b+", ");
+	public static void main(String[] args) {
+		int[] arr = { 1, 2, 3, 6, 7, 8, 10, 11, 12, 13, 14, 17, 18, 19, 20 };
+
+		for(int i=0;i<arr.length-1;i++) {
+
+			if(i<=4) {
+
+			String a= arr[i]+"-"+(arr[i+2]);
+
+			System.out.print(a+",");
+
 			i=i+2;
-			System.out.println(" 777777");
-		}else if(i<=10) {
-			System.out.println(" *****");
-			String c=a[i+1]+"-"+(a[i+5]);
-			System.out.print(c+", ");
+
+			}else if(i<=10) {
+
+			String a =arr[i]+"-"+(arr[i+4]);
+
+			System.out.print(a+",");
+
+			i=i+4; 
+			}else {
+
+			String a=arr[i]+"-"+(arr[i+3]);
+			System.out.print(a);
+			System.out.print(" "); 
 			i=i+4;
-			System.out.println();
-		}else {
-			
-			String d=a[i+1]+"-"+(a[i+4]);
-			System.out.print(d+", ");
-			i=i+4;
+				}
+			}
 		}
 	}
-}
-}
+
 //1-3,6-8,10-14,17-20
